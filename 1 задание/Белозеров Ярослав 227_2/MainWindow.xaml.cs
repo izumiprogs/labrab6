8be07b055d09c9f34fc207d9747001d0f36d4998
Ярослав <146277976+izumiprogs@users.noreply.github.com>
2023-12-06 12:20:52 +0300

    Add files via upload

---
+++ 1 задание/Белозеров Ярослав 227_2/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static System.Math;
+
+namespace Белозеров_Ярослав_227_2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        void Button_Click(object sender, EventArgs e)
+        {
+
+            double a = Convert.ToDouble(Rezult_a.Text);
+            if (a / 10 == a % 10)
+                TextBlockAnswer.Text = $"Цифры равны";
+            else
+                TextBlockAnswer.Text = $"Цифры не равны";
+        }
+    }
+}
